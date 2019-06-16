cd3cc4255df5dfeb250d2208364fef8338a2bc8e
Richard Afro <richyafro@hotmail.com>
2019-06-16 16:20:47 +0100

    home component

---
+++ src/components/home/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PostList from '../post-list';
+import './index.css';
+
+const Home = () => {
+  return (
+    <div className="home">
+      <PostList />
+    </div>
+  );
+};
+
+export default Home;
